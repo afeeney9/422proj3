add4a3328271ba99f101bc0fb423daa4c6d84e16
Ethan Gruening <ethgru@iastate.edu>
2025-03-27 13:13:07 -0500

    Fix to AWS import

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
-const {AWS} = require('aws-sdk');
+const AWS = require('aws-sdk');
 
 
 const app = express();
